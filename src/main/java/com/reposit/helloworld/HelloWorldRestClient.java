bd4ac4a64dd8ba4b79f976266265daa46e437ac9
Rob Nightingale <rob@robnightingale.net>
2018-01-09 11:34:03 +0000

    First commit of rest client

---
+++ src/main/java/com/reposit/helloworld/HelloWorldRestClient.java
@@ -0,0 +1,49 @@
+package com.reposit.helloworld;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
+
+import static spark.Spark.*;
+
+public class HelloWorldRestClient {
+
+    private final static Logger logger = LoggerFactory.getLogger(HelloWorldRestClient.class);
+
+    // this map is shared between sessions and threads, so it needs to be thread-safe (http://stackoverflow.com/a/2688817)
+    static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
+    static int nextUserNumber = 1; //Assign to username for next connecting user
+
+    public static void main(String[] args) {
+
+        get("/grpc-greeting", (rq, rs) -> {
+            HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+            try {
+                /* Access a service running on the local machine on port 50051 */
+                String user = "world";
+                if (args.length > 0) {
+                    user = args[0]; /* Use the arg as the name to greet if provided */
+                }
+                client.greet(user);
+                return rs;
+            } finally {
+                client.shutdown();
+            }
+        });
+
+        exception(Exception.class, (e, request, response) -> {
+            logger.error("Unexpected exception", e);
+            response.body("Error 500");
+        });
+
+    }
+
+}
