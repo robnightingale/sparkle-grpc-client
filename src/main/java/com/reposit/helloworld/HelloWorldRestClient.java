aafa69f944b4cc339c78bd560ac5cfd98f849428
Rob Nightingale <rob@robnightingale.net>
2018-01-11 10:09:15 +0000

    updated rest client

--- src/main/java/com/reposit/helloworld/HelloWorldRestClient.java
+++ src/main/java/com/reposit/helloworld/HelloWorldRestClient.java
@@ -23,17 +23,34 @@ public class HelloWorldRestClient {
     static int nextUserNumber = 1; //Assign to username for next connecting user
 
     public static void main(String[] args) {
+        staticFiles.location("/public"); //index.html is served at localhost:4567 (default port)
+        staticFiles.expireTime(600);
 
         get("/grpc-greeting", (rq, rs) -> {
             HelloWorldClient client = new HelloWorldClient("localhost", 50051);
             try {
                 /* Access a service running on the local machine on port 50051 */
-                String user = "world";
+                String user = "grpc world";
                 if (args.length > 0) {
                     user = args[0]; /* Use the arg as the name to greet if provided */
                 }
                 client.greet(user);
-                return rs;
+                return rs.body();
+            } finally {
+                client.shutdown();
+            }
+        });
+
+        get("/rest-greeting", (rq, rs) -> {
+            HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+            try {
+                /* Access a service running on the local machine on port 50051 */
+                String user = "rest world";
+                if (args.length > 0) {
+                    user = args[0]; /* Use the arg as the name to greet if provided */
+                }
+                client.greet(user);
+                return rs.body();
             } finally {
                 client.shutdown();
             }
